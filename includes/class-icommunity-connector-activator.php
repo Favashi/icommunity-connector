378b32c2d263490a5d4941781301240a0665dc70
favashi <favashi@gmail.com>
2023-07-06 13:46:21 +0200

    Initial module commit

---
+++ includes/class-icommunity-connector-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Icommunity_Connector
+ * @subpackage Icommunity_Connector/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Icommunity_Connector
+ * @subpackage Icommunity_Connector/includes
+ * @author     Toni Ruiz <info@toniruiz.es>
+ */
+class Icommunity_Connector_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-icommunity-connector-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Icommunity_Connector
+ * @subpackage Icommunity_Connector/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Icommunity_Connector
+ * @subpackage Icommunity_Connector/includes
+ * @author     Toni Ruiz <info@toniruiz.es>
+ */
+class Icommunity_Connector_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
